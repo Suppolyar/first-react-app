3d58a8ab72dc8a7ec7a0ee7f4746bbf42f405782
suppolyar <vkhardy@inbox.ru>
2021-05-28 22:29:49 +0300

    added application logic

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -5,7 +5,7 @@ import './app-header.css'
 const AppHeader = () => {
     return (
         <div className="app-header d-flex">
-            <h1>Ivan Petrychenko</h1>
+            <h1>Ilya Zakharov</h1>
             <h2>5 записей, из них понравилось 0</h2>
         </div>
     )
